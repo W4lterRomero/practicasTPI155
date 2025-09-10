1738d7e30ba13b9ed7b04143eeb60d3d29ac3426
W4lterRomero <rh23015@ues.edu.sv>
2025-09-10 15:19:24 -0600

    Practica de constructor en php

---
+++ Practicas_POO/app/constructor.php
@@ -0,0 +1,31 @@
+<?php
+class Carro {
+    public $color;
+    public $ruedas;
+    private $motor;
+    public $marca;
+    public $puertas;
+
+    function __construct($marca, $puertas, $color, $ruedas = 4, $motor = "1.6L") {
+        $this->color = $color;
+        $this->marca = $marca;
+        $this->puertas = $puertas;
+        $this->ruedas = $ruedas;
+        $this->motor = $motor; // como es privado, lo definimos aquí
+    }
+
+    function get_motor() {
+        return $this->motor;
+    }
+}
+
+// Crear un objeto
+$chevy = new Carro("Chevrolet", 4, "Rojo");
+
+// Probar
+echo "Marca: " . $chevy->marca . "<br>";
+echo "Color: " . $chevy->color . "<br>";
+echo "Puertas: " . $chevy->puertas . "<br>";
+echo "Ruedas: " . $chevy->ruedas . "<br>";
+echo "Motor: " . $chevy->get_motor();
+?>
