85e6bf280be39a33194082ddf08ddf0f9768d3f7
W4lterRomero <rh23015@ues.edu.sv>
2025-09-25 10:10:10 -0600

    Home controller y ruta/web en php

--- expresiones regulares/lib/Route.php
+++ expresiones regulares/lib/Route.php
@@ -4,7 +4,7 @@ namespace lib;
 
 class Route{
     private static $routes = [];
-    private static $URL_BASE="/mvc_clase/public";
+    private static $URL_BASE="/public";
 
 
     public static function get($uri, $callback){
@@ -14,7 +14,7 @@ class Route{
         self::$routes["POST"][self::$URL_BASE.$uri]=$callback;
     }
 
-
+/* 
    public static function dispatch(){
         $uri = $_SERVER["REQUEST_URI"];
        
@@ -32,13 +32,81 @@ class Route{
             if(preg_match("#^$url$#",$uri, $matches)){
                 $params = array_slice($matches,1);
                 //echo json_encode($params);
+                
                 $funcion(...$params);
                 return;
             }
         }
         echo "404";
     }
+ */
+/* 
+     public static function dispatch(){
+        $uri = $_SERVER["REQUEST_URI"];
+       
+        $method = $_SERVER["REQUEST_METHOD"];
+        //echo "Url".$uri."<br>";
+        //var_dump(self::$routes);
+        foreach(self::$routes[$method] as $url=>$funcion){
+            if(strpos($url, ":")!==false){
+                $url = preg_replace("#:[a-zA-Z]+#","([a-zA-Z]+)",$url);
+                //echo $url;
+                //return;
 
+            }
+
+
+            if(preg_match("#^$url$#",$uri, $matches)){
+                $params = array_slice($matches,1);
+                //echo json_encode($params);
+                $response = $funcion(...$params);
+
+
+                if(is_array($response) || is_object($response)){
+                    header("Content-Type: application/json");
+                    echo json_encode($response);
+                }
+                else{
+                    echo $response;
+                }
+                return;
+            }
+        }
+        echo "404";
+    }
+ */
+ public static function dispatch(){
+        $uri = $_SERVER["REQUEST_URI"];
+       
+        $method = $_SERVER["REQUEST_METHOD"];
+        //echo "Url".$uri."<br>";
+        //var_dump(self::$routes);
+        foreach(self::$routes[$method] as $url=>$funcion){
+            if(strpos($url, ":")!==false){
+                $url = preg_replace("#:[a-zA-Z]+#","([a-zA-Z]+)",$url);
+                //echo $url;
+                //return;
+            }
+
+
+            if(preg_match("#^$url$#",$uri, $matches)){
+                $params = array_slice($matches,1);
+                //echo json_encode($params);
+                $response = $funcion(...$params);
+
+
+                if(is_array($response) || is_object($response)){
+                    header("Content-Type: application/json");
+                    echo json_encode($response);
+                }
+                else{
+                    echo $response;
+                }
+                return;
+            }
+        }
+        echo "404";
+    }
 
 }
 
--- expresiones regulares/routes/web.php
+++ expresiones regulares/routes/web.php
@@ -12,6 +12,11 @@ Route::get("/", function(){
 Route::get("/inicio", function(){
     echo "Inicio";
 });
+//ruta con paramaetros para enviar a la funcion
+//el nombre de flag despues de lo s: tiene que ser el mismo que el que reciba la función
+Route::get("/inicio/:flag", function($flag){
+    return  array('nombre'=>"Walter");
+});
 
 
 Route::dispatch();
